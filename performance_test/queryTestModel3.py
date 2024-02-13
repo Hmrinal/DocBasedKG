ebcf693e83ce10204e3e62b4987a6cf123799ee2
Mrinal Tyagi <mrinaltyagi1912@gmail.com>
2024-02-13 19:26:37 +0100

    Update queryTestModel3.py

--- performance_test/queryTestModel3.py
+++ performance_test/queryTestModel3.py
@@ -2,11 +2,11 @@ from pymongo import MongoClient
 import time
 
 # Constants
-DATABASE_NAME = "29Dec_propertiesModel3"  # The actual database name
+DATABASE_NAME = "29Dec_propertiesModel3"  # The database name
 COLLECTION_NAME = "rdftojsonSegmentDoc"  # The collection to query
 QUERY = {"data_properties.Dec_DataProperties:hasBAT_NR": "48"}
 PROJECTION = {"_id": 1}
-ITERATIONS = 2000
+ITERATIONS = 100000
 
 # MongoDB Client Setup
 client = MongoClient('localhost', 27017)
@@ -29,6 +29,6 @@ def average_query_time(query, projection, iterations):
         total_time += duration
     return total_time / iterations
 
-# Running the query for 100 iterations and calculating average time
+# Running the query for 100,000 iterations and calculating average time
 avg_duration = average_query_time(QUERY, PROJECTION, ITERATIONS)
 print(f"Average query time over {ITERATIONS} iterations: {avg_duration:.4f} seconds.")
